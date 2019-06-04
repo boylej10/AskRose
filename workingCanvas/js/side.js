3275ee3003fb64bd75c31ae13de8d670c5144d0a
ch4r3l5 <juniormugisha@gmail.com>
2019-06-04 06:07:09 +0100

    Add files via upload

---
+++ workingCanvas/js/side.js
@@ -0,0 +1,7 @@
+var lc = LC.init(document.getElementById("lc"), {
+        imageURLPrefix: '../_assets/lc-images',
+        toolbarPosition: 'bottom',
+        defaultStrokeWidth: 2,
+        strokeWidths: [1, 2, 3, 5, 30]
+      });
+
